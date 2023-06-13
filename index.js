6b2273c3dd608c506937ee23f0c09dc87615b8fd
Elise McClain <celisemcclain@gmail.com>
2023-06-13 17:57:30 -0500

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42
+    return Math.abs(someValue - hqLocation)
+}
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue)
+    const feetPerBlock = 264
+    return blocks * feetPerBlock
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const difference = Math.abs(start - destination)
+    return difference * 264
+}
+distanceTravelledInFeet(42, 43)
+
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+        return 0
+    } if (distance > 400 && distance <= 2000) {
+        const pricePerFoot = 0.02
+        const minusFreeFeet = distance - 400
+        return pricePerFoot * minusFreeFeet
+    } if (distance > 2000 && distance <= 2500) {
+        return 25
+    } if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+    
+}
